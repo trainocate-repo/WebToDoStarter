c0ae92a97ab1e19a7ef73bba7bcf716919b31ccd
trainocate-repo <51771898+trainocate-repo@users.noreply.github.com>
2019-09-03 23:45:34 +0900

    不必要なSQLを削除

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,5 +6,3 @@ INSERT INTO `task_type` VALUES
 INSERT INTO `task` VALUES 
 (NULL,1,1,'JUnitを学習','テストの仕方を学習する','2020-07-07 15:00:00'),
 (NULL,1,3,'サービスの自作','マイクロサービスを作ってみる','2020-09-13 17:00:00');
-
-INSERT INTO `user` VALUES (1,'John','example@example.com','$2a$08$dSbk7S1tzk9I8CDB0mr6CO7ndTkTdC9/nojADBiukhWVBgHqR/g46',1,'something','temp');
